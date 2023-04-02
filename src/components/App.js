ce7b43ae47f1ada283389316cb7dadac6d3e213a
Saurabh Patil <119738647+saurabhpatil90@users.noreply.github.com>
2023-04-02 15:34:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
   const [loading, setLoading] = useState(true);
- const API_KEY="b9eea9ddd4dbb47bf0bb1dfc76109bda";
+ const API_KEY="b9eea9ddd4dbb47bf0bb5dfc76109bda";
 
  const changeFunction=(e)=>
  {
